774e416fa6dce971e8adbc37761550f8b906c741
Alejandro Aponte <yasmiverromero@gmail.com>
2024-07-30 16:31:42 -0500

    ahora lo ajustes que se hacen en los filtros ya sepueden ver :)

--- HojaR.js
+++ HojaR.js
@@ -6,11 +6,22 @@ function GenerarMatriz() {
     var filas = parseInt(document.getElementById('Filas').value);
     var columnas = parseInt(document.getElementById('Columnas').value);
 
-    // Verifica si las filas y columnas son válidas
-    /* if (isNaN(filas) || filas == 0 || isNaN(columnas) || columnas == 0) {
-        alert('Debes seleccionar números mayores a cero.');
-        return;
-    } */
+    //obtener el resultdo de los checkbox
+    var acordemenor = document.getElementById('AMenores').checked;
+    var sostenidos = document.getElementById('ASostenidos').checked;
+    var bemoles = document.getElementById('ABemoles').checked;
+    
+    var funcionAcordes = [];
+    if (acordemenor) {
+        funcionAcordes.push(MenoresA);
+    }
+    if (sostenidos) {
+        funcionAcordes.push(SostenidosA);
+    }
+    if (bemoles) {
+        funcionAcordes.push(BemolesA);
+    }
+    funcionAcordes.push(HRandom)
 
 
     //genero la matriz
@@ -21,8 +32,9 @@ function GenerarMatriz() {
         var filaM = [];
         //con el segundo for iteramos las columnas y llenar cada fila
         for (let c = 0; c < columnas; c++) {
-            //con lo que me retorna HRandom con el método push lo añado a mi subarreglo fila  
-            filaM.push(HRandom());
+            var AcordesElegidos = funcionAcordes[Math.floor(Math.random() * funcionAcordes.length)];
+            var acorde = AcordesElegidos();
+            filaM.push(acorde);        
         }
         //una vez llenada una fila 'fila[]' la enviamos con el método push
         hoja.push(filaM);
@@ -63,4 +75,22 @@ function HRandom() {
     var acorde = Math.floor(Math.random() * generar.length);
     //Ahora acode tiene el indice de generar y con return puede retornar la letra segun el indice. 
     return generar[acorde];
+}
+
+function MenoresA() {
+    var acordeMenor = ['Cm', 'Dm', 'Em', 'Fm', 'Gm', 'Am', 'Bm'];
+    var AMenor = Math.floor(Math.random() * acordeMenor.length);
+    return acordeMenor[AMenor];
+}
+
+function SostenidosA() {
+    var acordeMenor = ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'];
+    var AMenor = Math.floor(Math.random() * acordeMenor.length);
+    return acordeMenor[AMenor];
+}
+
+function BemolesA() {
+    var acordeMenor = ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb'];
+    var AMenor = Math.floor(Math.random() * acordeMenor.length);
+    return acordeMenor[AMenor];
 }
