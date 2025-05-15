9f3f8ab841013c66079c4e141140fba2bd9acd0a
Felipe <bohorquezfelipe85@gmail.com>
2025-05-14 22:01:15 -0500

    Se añadio el grado G

---
+++ src/escalaG.ts
@@ -0,0 +1,69 @@
+import { nodo } from "./nodo.js";
+
+export class escalaG {
+    raiz: nodo;
+
+    constructor() {
+        this.raiz = this.crearArbol(); // Asignación del árbol fijo
+    }
+
+    private crearArbol(): nodo {
+        const nodo1 = new nodo("G", "I");
+        const nodo2 = new nodo("Am", "ii");
+        const nodo3 = new nodo("Bm", "iii");
+        const nodo4 = new nodo("C", "IV");
+        const nodo5 = new nodo("D", "V");
+        const nodo6 = new nodo("Em", "vi");
+        const nodo7 = new nodo("F#°", "vii°");
+
+        nodo2.izquierda = nodo1;
+        nodo2.derecha = nodo3;
+
+        nodo6.izquierda = nodo5;
+        nodo6.derecha = nodo7;
+
+        nodo4.izquierda = nodo2;
+        nodo4.derecha = nodo6;
+
+        return nodo4;
+    }
+
+    inOrden(nodo: nodo | null): void {
+        if (nodo !== null) {
+            this.inOrden(nodo.izquierda);
+            console.log(`Grado: [${nodo.grado}] -> Acorde: ${nodo.nombreChord}`);
+            this.inOrden(nodo.derecha);
+        }
+    }
+
+    buscar(grado: string): string {
+        return this.buscarRec(this.raiz, grado);
+    }
+
+    private buscarRec(nodo: nodo | null, grado: string): string {
+        if (nodo === null) {
+            return "El grado no corresponde a ningún acorde";
+        }
+
+        if (grado === nodo.grado) {
+            return `El acorde: ${nodo.nombreChord} es correcto`;
+        }
+
+        if (grado < nodo.grado) {
+            return this.buscarRec(nodo.izquierda, grado);
+        } else {
+            return this.buscarRec(nodo.derecha, grado);
+        }
+    }
+}
+
+// Prueba
+/* function main(): void {
+    const arbol = new escalaC();
+    arbol.inOrden(arbol.raiz);
+    console.log(" ");
+    console.log(arbol.buscar("I"));
+    console.log(arbol.buscar("8"));
+}
+
+main(); */
